bc68091136149c9c189c2a7adeaa3a31d7c58e69
digisora <jonkhia@gmail.com>
2016-09-24 23:09:06 +0000

    Set up comments in db

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,9 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :description, :address, presence: true
   validates :name, presence: true, length: { minimum: 4 }
 
